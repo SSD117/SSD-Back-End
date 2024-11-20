f52fa02af77aeb565c091f18ec1189de9cf66eec
5un6h33 <pinedelta@icloud.com>
2024-11-20 13:38:58 +0900

    fix: deleteClass

--- src/controllers/class.controller.js
+++ src/controllers/class.controller.js
@@ -15,14 +15,18 @@ const registerClass = async (req, res) => {
     await Class.registerClass(req.session.user_id, req.query.class_id);
     return res.status(201).send({ message: "스포츠 강좌 등록 성공" });
   } catch (err) {
+    if (err.message == `Duplicate entry '1-1' for key 'class.PRIMARY'`)
+      return res
+        .status(400)
+        .send({ message: `class_id: ${req.query.class_id}이 중복됩니다` });
     console.log(err);
-    return res.status(400).send(err);
+    return res.status(400).send(err.message);
   }
 };
 
 const deleteClass = async (req, res) => {
   try {
-    await Class.deleteClass(req.session.user_id, req.query.class_id);
+    await Class.dropClass(req.session.user_id, req.query.class_id);
     return res.status(200).send({ message: "스포츠 강좌 취소 성공" });
   } catch (err) {
     console.log(err);
@@ -44,6 +48,10 @@ const addBookmark = async (req, res) => {
     await Class.addBookmark(req.session.user_id, req.query.class_id);
     return res.status(201).send({ message: "즐겨찾기 등록 성공" });
   } catch (err) {
+    if (err.message == `Duplicate entry '1-1' for key 'bookmark.PRIMARY'`)
+      return res
+        .status(400)
+        .send({ message: `class_id: ${req.query.class_id}이 중복됩니다` });
     console.log(err);
     return res.status(400).send(err);
   }
